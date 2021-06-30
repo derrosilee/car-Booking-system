77ae15f80694fcfacc9be96bc59551d8d0151665
derrosilee <derrosilee@gmail.com>
2021-06-30 14:52:31 +0300

    Added Mobile Responsive Breadcrumb for Smaller Devices

---
+++ src/Auto/static/Auto/js/main.js
@@ -0,0 +1,13 @@
+var isActive = false;
+
+$('.menu').on('click', function () {
+    if (isActive) {
+        $(this).removeClass('active');
+        $('body').removeClass('menu-open');
+    }   else{
+        $(this).addClass('active');
+        $('body').addClass('menu-open')
+    }
+
+    isActive = !isActive;
+});
